2b304bf9aa9040a8fe65746c41a988eebd2e1792
kosovic-drasko <98913762+kosovic-drasko@users.noreply.github.com>
2025-03-21 08:11:18 +0100

    init

--- src/PropsChildren/Parent.js
+++ src/PropsChildren/Parent.js
@@ -5,7 +5,7 @@ import Child from './Child'
 export default function Parent() {
   return (
     <>
-      <Child ime='ana'><p>textxt</p></Child>
+      <Child ime='ana'><p>children text</p></Child>
       <div>Parent</div>
     </>
   )
